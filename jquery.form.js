7f7ed80a5f3e5f091be697ddca75b51ff075d506
Andrew Dodson <sonofdod@gmail.com>
2012-07-01 22:31:03 +1000

    Test for input[type=date]

--- jquery.form.js
+++ jquery.form.js
@@ -4,45 +4,37 @@
  *  @since Oct 2011 (that's refreshing!)
  */
 (function($){
-
+ 
 	"use strict";
 
 	// test for support
-	$.support.placeholder = (function(){
-		var test = document.createElement('input');
-		return ('placeholder' in test);
-	})();
+	$.support.placeholder = test('input[placeholder=wicked]');
+	$.support.datalist = test('input[list=hello]');
+	$.support.range = test('input[type=range]');
+	$.support.number = test('input[type=number]');
+	$.support.date = test('input[type=date]');
 
-	// test for support
-	$.support.datalist = (function(){
-		var test = document.createElement('input');
-		test.setAttribute('list','hello');
-		return ('list' in test);
-	})();
+	// inputSupport
+	function test(s){
 
-	// test for support
-	$.support.range = (function(){
-		var test = document.createElement('input');
+		var m = s.match(/^([a-z]+)(\[([a-z]+)(\=([a-z]+))?\])?$/i);
+
 		try{
-			test.type = 'range';
+			var test = document.createElement(m[1]);
+
+			if(m[3]&&m[5]){
+				test[m[3]] = m[5];
+				return test[m[3]] === m[5];
+			}
+			else if(m[3]){
+				return m[3] in test;
+			}
 		}
 		catch(e){
 			return false;
 		}
-		return test.type === 'range';
-	})();
-
-	// test for support
-	$.support.number = (function(){
-		var test = document.createElement('input');
-		try{
-			test.type = 'number';
-		}
-		catch(e){
-			return false;
-		}
-		return test.type === 'number';
-	})();
+		return true;
+	}
 
 
 	// local root
@@ -64,6 +56,7 @@
 			$(this).trigger('up');
 		}
 	});
+
 
 	// the interval would be better if it was per input
 	var interval;
@@ -377,90 +370,93 @@
 	/**
 	 * Calendar
 	 */
-	$("form input[type=date]").live("focus select", function(){
-	
-		var $calendar = $("+ div.calendar div", this).fadeIn('fast')
-			,days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-			,month= ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-			,$input = $(this);
+	if(!$.support.date){
+
+		$("form input[type=date]").live("focus select", function(){
 		
-		if(!$calendar.length){
-			$calendar = $('<div class="calendar"><div></div></div>').insertAfter(this).find('div');
-		}
-
-		// trigger close calendar when clicked outside
-		$(this).blur(function(){$calendar.fadeOut('fast');});
-		
-		var d = new Date();	
-		if($(this).val().match(/^[0-9]{4}\-[0-1]?[0-9]\-[0-3]?[0-9]$/)){
-			var a = $(this).val().split("-").slice(0,3);
-			d.setYear(a[0]);
-			d.setDate(a[2]);
-			d.setMonth(--a[1]);
-		}
-
-		// print the headline
-		(function createcalendar(d){
-			var s='<table><caption></caption><thead><tr>';
-			for(var x in days){
-			   s += "<th>"+days[x]+"<\/th>";
-			}
-			s+='</tr></thead><tbody>';
-			var dom = d.getDate();// user selected dom(day of month)?
-			d.setDate(1);
-			// pad the table
-			var dow = d.getDay();// first dom falls on a...
-			if(dow>0){
-				s += "<tr><td colspan='"+dow+"'>&nbsp;<\/td>";
-			}
-			// get the last day of the month
-			d.setMonth(d.getMonth()+1);
-			d.setDate(0);
+			var $calendar = $("+ div.calendar div", this).fadeIn('fast')
+				,days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+				,month= ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+				,$input = $(this);
 			
-			for(var j=1;j<=d.getDate();j++){
-			   s += ((dow+j-1)%7===0?'<tr>':'') 
-					   + "<td"+(dom===j?' class="selected"':'')
-					   +  "><a href='javascript:void(0);'>"+j+"<\/a><\/td>" 
-					   + ((dow+j)%7===0?'</tr>':'');
+			if(!$calendar.length){
+				$calendar = $('<div class="calendar"><div></div></div>').insertAfter(this).find('div');
 			}
-			// pad the table
-			if(d.getDay()<6){
-			   s += "<td colspan='"+(6-d.getDay())+"'>&nbsp;<\/td><\/tr>";
-			}
-			s+='<\/tbody><\/table>';
-			
-			// create the calendar and add events
-			$calendar.empty().append('<a href="javascript:void(0);" class="close">close</a>').find('a').click(function(){
-				$calendar.fadeOut('fast');
-			});
 
-			$(s)
-				.prependTo($calendar)
-				.find('td')
-				.click(function(){
-					var s = $(this).text();
-					$input.val(d.getUTCFullYear()+'-'+( (d.getMonth()+1) < 10 ? '0' : '' ) + (d.getMonth()+1)+'-'+( s < 10 ? '0' : '' ) + s );
-					$input.trigger('blur');
+			// trigger close calendar when clicked outside
+			$(this).blur(function(){$calendar.fadeOut('fast');});
+			
+			var d = new Date();	
+			if($(this).val().match(/^[0-9]{4}\-[0-1]?[0-9]\-[0-3]?[0-9]$/)){
+				var a = $(this).val().split("-").slice(0,3);
+				d.setYear(a[0]);
+				d.setDate(a[2]);
+				d.setMonth(--a[1]);
+			}
+
+			// print the headline
+			(function createcalendar(d){
+				var s='<table><caption></caption><thead><tr>';
+				for(var x in days){
+				   s += "<th>"+days[x]+"<\/th>";
+				}
+				s+='</tr></thead><tbody>';
+				var dom = d.getDate();// user selected dom(day of month)?
+				d.setDate(1);
+				// pad the table
+				var dow = d.getDay();// first dom falls on a...
+				if(dow>0){
+					s += "<tr><td colspan='"+dow+"'>&nbsp;<\/td>";
+				}
+				// get the last day of the month
+				d.setMonth(d.getMonth()+1);
+				d.setDate(0);
+				
+				for(var j=1;j<=d.getDate();j++){
+				   s += ((dow+j-1)%7===0?'<tr>':'') 
+						   + "<td"+(dom===j?' class="selected"':'')
+						   +  "><a href='javascript:void(0);'>"+j+"<\/a><\/td>" 
+						   + ((dow+j)%7===0?'</tr>':'');
+				}
+				// pad the table
+				if(d.getDay()<6){
+				   s += "<td colspan='"+(6-d.getDay())+"'>&nbsp;<\/td><\/tr>";
+				}
+				s+='<\/tbody><\/table>';
+				
+				// create the calendar and add events
+				$calendar.empty().append('<a href="javascript:void(0);" class="close">close</a>').find('a').click(function(){
 					$calendar.fadeOut('fast');
-				})
-				.end()
-				.find('caption')
-				.append('<a href="javascript:void(0);" class="prev">'+ month[d.getMonth()==0?11:(d.getMonth()-1)] +'</a> <span class="current">'+ month[d.getUTCMonth()] + ' ' + d.getUTCFullYear() +'</span> <a href="javascript:void(0);" class="next">'+ month[(d.getMonth()+1)%12] +'</a>')
-				.find('a')
-				.click(function(){
-					if(this.className==='current'){
-						//$input.val(d.getUTCFullYear()+'-'+(d.getMonth()+1));
-						return false;
-					}
-					$calendar.fadeIn('fast');
-					d.setDate(1);
-					d.setMonth((d.getMonth()+{'next':1,'prev':-1}[this.className]));
-					log(d);
-					createcalendar(d);
-					return false;
 				});
-		})(d);
-	});
+
+				$(s)
+					.prependTo($calendar)
+					.find('td')
+					.click(function(){
+						var s = $(this).text();
+						$input.val(d.getUTCFullYear()+'-'+( (d.getMonth()+1) < 10 ? '0' : '' ) + (d.getMonth()+1)+'-'+( s < 10 ? '0' : '' ) + s );
+						$input.trigger('blur');
+						$calendar.fadeOut('fast');
+					})
+					.end()
+					.find('caption')
+					.append('<a href="javascript:void(0);" class="prev">'+ month[d.getMonth()==0?11:(d.getMonth()-1)] +'</a> <span class="current">'+ month[d.getUTCMonth()] + ' ' + d.getUTCFullYear() +'</span> <a href="javascript:void(0);" class="next">'+ month[(d.getMonth()+1)%12] +'</a>')
+					.find('a')
+					.click(function(){
+						if(this.className==='current'){
+							//$input.val(d.getUTCFullYear()+'-'+(d.getMonth()+1));
+							return false;
+						}
+						$calendar.fadeIn('fast');
+						d.setDate(1);
+						d.setMonth((d.getMonth()+{'next':1,'prev':-1}[this.className]));
+						log(d);
+						createcalendar(d);
+						return false;
+					});
+			})(d);
+		});
+	}
 
 
 	/**
